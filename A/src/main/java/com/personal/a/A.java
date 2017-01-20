ab37dffd9b75de0e21ac874a7aebd250ca666319
sundayliu <martinkro@aliyun.com>
2017-01-20 18:01:00 +0800

    A'

---
+++ A/src/main/java/com/personal/a/A.java
@@ -0,0 +1,8 @@
+package com.personal.a;
+
+class A{
+    public static final String version = "1.0.1(2017/01/20)";
+    public static void Main(String[] args){
+        System.out.println("A version:" + version);
+    }
+}
